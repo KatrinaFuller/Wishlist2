8bb1bcfed603040f124fd37914fb56a1e03332b1
Katrina Fuller <trinafuller@att.net>
2019-11-04 18:36:42 -0700

    filled out wishesController

--- Controllers/WishesController.cs
+++ Controllers/WishesController.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using Wishlist2.Models;
+using Wishlist2.Services;
 
 namespace Wishlist2.Controllers
 {
@@ -6,6 +11,95 @@ namespace Wishlist2.Controllers
   [Route("api/[controller]")]
   public class WishesController : ControllerBase
   {
+    private readonly WishesService _ws;
+    public WishesController(WishesService ws)
+    {
+      _ws = ws;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Wish>> Get()
+    {
+      try
+      {
+        return Ok(_ws.Get());
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+
+      }
+    }
+
+    [HttpGet("user")]
+    public ActionResult<IEnumerable<Wish>> GetWishesByUserId()
+    {
+      try
+      {
+        var id = HttpContext.User.FindFirstValue("Id");
+        return Ok(_ws.GetWishesByUserId(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Wish> GetWishByWishId(int id)
+    {
+      try
+      {
+        return Ok(_ws.GetWishByWishId(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<Wish> Create([FromBody] Wish newWish)
+    {
+      try
+      {
+        newWish.UserId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_ws.Create(newWish));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Wish> Edit([FromBody] Wish editWish, int id)
+    {
+      try
+      {
+        editWish.Id = id;
+        return Ok(_ws.Edit(editWish));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<int> Delete(int id)
+    {
+      try
+      {
+        var userId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_ws.Delete(id, userId));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
 
   }
 }
--- Services/WishesService.cs
+++ Services/WishesService.cs
@@ -1,7 +1,38 @@
+using System;
+using Wishlist2.Models;
+
 namespace Wishlist2.Services
 {
-    public class WishesService
+  public class WishesService
+  {
+    internal object Get()
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal object GetWishesByUserId(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object GetWishByWishId(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Create(Wish newWish)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Edit(Wish editWish)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Delete(int id, string userId)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
